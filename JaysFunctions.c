26b33302a77df51b51300b5a8229a7cea07b2ff8
Jay Tilman <jtilman@nevada.unr.edu>
2024-05-06 04:47:12 +0000

    I finally got the loadImage function to workgit add .git add .git add .git add .git add .git add .

--- JaysFunctions.c
+++ JaysFunctions.c
@@ -5,9 +5,10 @@
 #include <stdio.h>
 #define IMAGE "image.txt"
 #define SIZE 500
+#define NAMECAP 50
 #include <stdbool.h>
 
-void loadNewImage(FILE* filePtr, int rowSize, int colSize, int imageArray[][colSize], int* trueRowPtr, int* trueColPtr);
+void loadNewImage(char fileName[], int rowSize, int colSize, int imageArray[][colSize], int* trueRowPtr, int* trueColPtr);
 void displayEditMenu();
 int getEditChoice();
 void getCropSpecs(int* startRowPtr, int* endRowPtr, int* startColPtr, int* endColPtr);
@@ -15,39 +16,69 @@ void saveNewImage(FILE* filePtr, int rowSize, int colSize, int imageArray[][colS
 void sayGoodbye();
 
 int main() {
-	FILE* loadFile;
+	char loadFile[NAMECAP];
 	int anArray[SIZE][SIZE];
 	int trueRowSize, trueColSize;
 	
-	loadFile = fopen(IMAGE, "r");
-	if(loadFile == NULL){
-		printf("AHHHH\n");
-		return 0;
-	}
-	loadNewImage(loadFile, SIZE, SIZE, anArray, &trueRowSize, &trueColSize);  
+	
+	loadNewImage(IMAGE, SIZE, SIZE, anArray, &trueRowSize, &trueColSize);  
 	
 	printf(" %d, %d\n", trueRowSize, trueColSize);
 	
-	fclose(loadFile);
 	
 	return 0;
 }
 		
-void loadNewImage(FILE* filePtr, int rowSize, int colSize, int imageArray[][colSize], int* trueRowPtr, int* trueColPtr){
-	int count, row, col, tRow, tCol, columnCount[colSize];
-	count = tRow = tCol = 0;
+void loadNewImage(char fileName[], int rowSize, int colSize, int imageArray[][colSize], int* trueRowPtr, int* trueColPtr){
+	FILE* file;
+	int rowCount, colCount, elementCount, rowArray[rowSize][colSize];
+	rowCount = colCount = elementCount = 0;
 	
-	for(row = 0; row < rowSize; row++){
-		for(col = 0; col < colSize; col++){
-			while(fscanf(filePtr, "%d", &imageArray[row][col]) == 1){
-				tRow++;
+	file = fopen(fileName, "r");
+	if(file != NULL){
+	
+		for(int e = 0; e < colSize*rowSize; e++){
+			while(fscanf(file, "%1d", &e) == 1){
+				elementCount++;
 			}
-			
 		}
 	}
-	tCol = count / tRow;
-	*trueRowPtr = tRow;
-	*trueColPtr = tCol;
+	fclose(file);
+	
+	file = fopen(fileName, "r");
+	if(file != NULL){
+		for(int row = 0; row < rowSize; row++){
+			for(int col = 0; col < colSize; col++){
+				while(fscanf(file, "%d", &rowArray[row][col]) == 1){
+					rowCount++;
+				}
+			}
+		}
+	}
+	fclose(file);
+	
+	colCount = elementCount / rowCount;
+	
+	file = fopen(fileName, "r");
+	if(file != NULL){
+		for(int row = 0; row < rowCount; row++){
+			for(int col = 0; col < colCount; col++){
+				fscanf(file, "%1d", &imageArray[row][col]);
+			}
+		}
+	}
+	fclose(file);
+	
+	for(int row = 0; row < 12; row++){
+		for(int col = 0; col < 21; col++){
+			printf("%d", imageArray[row][col]);
+		}
+		printf("\n");
+	}
+	
+	*trueRowPtr = rowCount;
+	*trueColPtr = colCount;
+	
 }
 
 void displayEditMenu(){
@@ -98,9 +129,6 @@ void saveNewImage(FILE* filePtr, int rowSize, int colSize, int imageArray[][colS
 void sayGoodbye(){
 	printf("Goodbye!\n");
 }
-
-
-
 
 
 
